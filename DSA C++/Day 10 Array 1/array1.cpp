c2eb8da307c9c3f3d8b09183322199973fb50f18
Abhijeet Kumar <abhijeetkumart1@gmail.com>
2025-07-15 12:36:04 +0530

    Auto push - 15-07-2025

---
+++ DSA C++/Day 10 Array 1/array1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //array declare
+    int arr[7];
+    cout<<arr<<endl;
+    cout<<&arr<<endl;
+    cout<<"Array Declare Successfully"<<endl;
+    return 0;
+}
