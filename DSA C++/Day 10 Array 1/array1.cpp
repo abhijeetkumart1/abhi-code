9f3337b4c034f75ee3b0ee19906f6eec6d1436c7
Abhijeet Kumar <abhijeetkumart1@gmail.com>
2025-07-16 15:17:06 +0530

    Auto push - 16-07-2025

--- DSA C++/Day 10 Array 1/array1.cpp
+++ DSA C++/Day 10 Array 1/array1.cpp
@@ -1,10 +1,51 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     //array declare
+//     int arr[7];
+//     cout<<arr<<endl;
+//     cout<<&arr<<endl;
+//     cout<<"Array Declare Successfully"<<endl;
+//     return 0;
+// }  
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[]={1,3,5,7,9};
+//     cout<<arr[4]<<endl;
+//     //printing all values
+//     for(int i=0;i<5;i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+//     int arr[10];
+//     cout<<"Enter the input values in array "<<endl;
+//     for(int i=0;i<10;i++){
+//         cin>>arr[i];
+//     }
+//     cout<<"printing the values of array"<<endl;
+//     for (int i = 0; i <10; i++){
+//         cout<<arr[i]<<" ";
+
+//     }
+    
+
+//     return 0;
+// }
+//Take  5 element in input and print their double
 #include<iostream>
 using namespace std;
 int main(){
-    //array declare
-    int arr[7];
-    cout<<arr<<endl;
-    cout<<&arr<<endl;
-    cout<<"Array Declare Successfully"<<endl;
-    return 0;
+    int arr[5];
+    cout << "Enter 5 elements:" << endl;
+    for(int i=0; i<5; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Doubles of the elements:" << endl;
+    for(int i=0; i<5; i++){
+        cout << 2 * arr[i] << " ";
+    }
+    cout << endl;
 }
---
+++ WEBDEV/Javascript/Javascript 1/index.js
@@ -0,0 +1,4 @@
+//Line below is used to print on the console window
+console.log("Hello World");
+let a=10;
+console.log(a);
